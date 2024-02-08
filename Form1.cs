bb1edd441e316e828835e711e6fcf1c1d4ad3bd2
NIKKnak <nik88350759@yandex.ru>
2024-02-08 15:01:26 +0300

    работает

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 using System.IO.Compression;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
 
 namespace ConvertAndZip
 {
@@ -15,37 +16,56 @@ namespace ConvertAndZip
         {
             FolderBrowserDialog dialog = new FolderBrowserDialog();
 
-
+/*            Timer timer = new Timer();
+            timer.Interval = 5000;
+            timer.Start();*/
 
 
 
             if (dialog.ShowDialog() == DialogResult.OK)
             {
+                string pathDirrectory = dialog.SelectedPath;
 
-                    string pathDirrectory = dialog.SelectedPath;
 
-                while (Directory.GetFiles(pathDirrectory, "*.xps").Length > 0)
+
+
+
+                //Directory.GetFiles(pathDirrectory, "*.xps").Length > 0
+
+
+                while (true)
                 {
 
-
-
-                    string[] dirrectoriesName = Directory.GetDirectories(pathDirrectory);
-                    string[] fileNames = Directory.GetFiles(pathDirrectory);
-
-                    string dirrectoryName = Path.GetFileName(dirrectoriesName[0]);
-                    string fileName = Path.GetFileNameWithoutExtension(fileNames[0]);
-
-                    string fileNameForTransmission = fileNames[0];// костыль
-
-                    if (dirrectoryName == fileName)
+                    try
                     {
-                        CopyTextFile copyTextFile = new CopyTextFile();
-                        copyTextFile.ConvertFile(fileNameForTransmission);
 
-                        ZipArhiv zipArhiv = new ZipArhiv();
-                        zipArhiv.zip(pathDirrectory);
+                        string[] dirrectoriesName = Directory.GetDirectories(pathDirrectory);
+                        string[] fileNames = Directory.GetFiles(pathDirrectory);
+
+                        string dirrectoryName = Path.GetFileName(dirrectoriesName[0]);
+                        string fileName = Path.GetFileNameWithoutExtension(fileNames[0]);
+
+                        string fileNameForTransmission = fileNames[0];// костыль
+
+                        if (dirrectoryName == fileName)
+                        {
+                            CopyTextFile copyTextFile = new CopyTextFile();
+                            copyTextFile.ConvertFile(fileNameForTransmission);
+
+                            ZipArhiv zipArhiv = new ZipArhiv();
+                            zipArhiv.zip(pathDirrectory);
+
+                        }
+
 
                     }
+                    catch {
+                        Thread.Sleep(5000);
+                    
+                    }
+
+
+                    
 
 
 
@@ -53,10 +73,10 @@ namespace ConvertAndZip
 
 
 
-                
 
 
-                
+
+
 
 
 
